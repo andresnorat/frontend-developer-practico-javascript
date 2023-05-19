75f37c17633a4fb07c2bc9fdc2fd23bcea3a3faf
Andres Norato <carnorat@gmail.com>
2023-05-19 15:20:44 -0500

    fusion del mobile-menu

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
+const burguerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 navEmail.addEventListener('click', toogleDesktopMenu)
-
+burguerIcon.addEventListener('click', toogleMobileMenu)
 
 function toogleDesktopMenu (){
     desktopmenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu (){
+    mobileMenu.classList.toggle('inactive');
 }
